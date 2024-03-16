6e08ee69848b7d74e28e069b6595a46185344f97
tom1993415 <tom1993415@yahoo.com.tw>
2024-03-16 23:00:21 +0800

    20240316 作業

---
+++ Embedded Systems/project/Core/Inc/usermain.h
@@ -0,0 +1,16 @@
+#ifndef USERMAIN_H
+#define USERMAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function prototypes
+void usermain_init(void);
+void usermain_loop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USERMAIN_H */
